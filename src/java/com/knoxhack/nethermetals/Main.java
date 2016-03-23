cc60462b4d5c5cf776eb197e73c0714666d6b648
knoxhack <hackofgod@gmail.com>
2016-03-23 19:32:11 -0400

    mod frame
    mod frame

---
+++ src/java/com/knoxhack/nethermetals/Main.java
@@ -0,0 +1,51 @@
+package com.knoxhack.nethermetals;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
+public class Main {
+
+    public static final String MODID = "nethermetals";
+    public static final String MODNAME = "Nether Metals";
+    public static final String VERSION = "1.0.0";
+
+    @Instance
+    public static Main instance = new Main();
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+        Main.proxy.preInit(e);
+    	
+    	
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+        Main.proxy.init(e);
+    	
+    	
+    	
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+        Main.proxy.postInit(e);
+    	
+    	
+    	
+    	
+    }
+    
+    @SidedProxy(clientSide="com.knoxhack.nethermetals.ClientProxy", serverSide="com.knoxhack.nethermetals.ServerProxy")
+    public static CommonProxy proxy;
+    
+    
+    
+    
+}
