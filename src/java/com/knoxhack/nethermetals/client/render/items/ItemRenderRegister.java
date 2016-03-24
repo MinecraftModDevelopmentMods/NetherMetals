fa4aeb0cdb085b63d297de96669ce98fa088f474
knoxhack <hackofgod@gmail.com>
2016-03-23 20:25:40 -0400

    Build #3
    Build #3

--- src/java/com/knoxhack/nethermetals/client/render/items/ItemRenderRegister.java
+++ src/java/com/knoxhack/nethermetals/client/render/items/ItemRenderRegister.java
@@ -1,5 +1,24 @@
 package com.knoxhack.nethermetals.client.render.items;
 
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+
+import com.knoxhack.nethermetals.Main;
+import com.knoxhack.nethermetals.items.ModItems;
+
 public final class ItemRenderRegister {
 
+	public static void registerItemRenderer() {
+		reg(ModItems.nethersword);
+	}
+
+	//==========================================================================
+
+	public static String modid = Main.MODID;
+
+	public static void reg(Item item) {
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(modid + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+	}
 }
