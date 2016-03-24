a0c65c43ebedbd04b812682d692fc2201be504b1
knoxhack <hackofgod@gmail.com>
2016-03-23 22:37:34 -0400

    Build #6
    Ores added

--- src/java/com/knoxhack/nethermetals/blocks/ModBlocks.java
+++ src/java/com/knoxhack/nethermetals/blocks/ModBlocks.java
@@ -6,9 +6,31 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public final class ModBlocks {
 
     public static Block nethercopperOre;
+    public static Block nethertinOre;
+    public static Block netherleadOre;
+    public static Block nethersilverOre;
+    public static Block nethernickelOre;
+    public static Block netherzincOre;
 
 
     public static void createBlocks() {
-        GameRegistry.registerBlock(nethercopperOre = new BasicBlock("Nether_Copper_Ore"), "Nether_Copper_Ore");
+        GameRegistry.registerBlock(nethercopperOre = new BasicBlock("nether_copper_ore"), "nether_copper_ore");
+        GameRegistry.registerBlock(nethertinOre = new BasicBlock("nether_tin_ore"), "nether_tin_ore");
+        GameRegistry.registerBlock(netherleadOre = new BasicBlock("nether_lead_ore"), "nether_lead_ore");
+        GameRegistry.registerBlock(nethersilverOre = new BasicBlock("nether_silver_ore"), "nether_silver_ore");
+        GameRegistry.registerBlock(nethernickelOre = new BasicBlock("nether_nickel_ore"), "nether_nickel_ore");
+        GameRegistry.registerBlock(netherzincOre = new BasicBlock("nether_zinc_ore"), "nether_zinc_ore");
+
+    
+    
+    
+    
+    
+    
+    
+    
     }
+    
+    
+    
 }
--- src/java/com/knoxhack/nethermetals/client/render/blocks/BlockRenderRegister.java
+++ src/java/com/knoxhack/nethermetals/client/render/blocks/BlockRenderRegister.java
@@ -9,14 +9,25 @@ import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 
 public final class BlockRenderRegister {
-	public static String modid = Main.MODID;
 
 	public static void registerBlockRenderer() {
-	    reg(ModBlocks.nethercopperOre);
+		reg(ModBlocks.nethercopperOre);
+		reg(ModBlocks.nethertinOre);
+		reg(ModBlocks.netherleadOre);
+		reg(ModBlocks.nethersilverOre);
+		reg(ModBlocks.nethernickelOre);
+		reg(ModBlocks.netherzincOre);
+
+
+
+
+
 	}
 
+	public static String modid = Main.MODID;
+
 	public static void reg(Block block) {
-	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
-	    .register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(modid + ":" + block.getUnlocalizedName().substring(5), "inventory"));
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(modid + ":" + block.getUnlocalizedName().substring(5), "inventory"));
 	}
+
 }
