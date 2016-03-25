0dffbc70bdc4692726438bcc6a30a362346a80fa
knoxhack <hackofgod@gmail.com>
2016-03-25 17:19:23 -0400

    more stuff

--- src/main/java/com/knoxhack/nethermetals/Main.java
+++ src/main/java/com/knoxhack/nethermetals/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static final String MODID = "nethermetals";
     public static final String MODNAME = "Nether Metals";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     @Instance
     public static Main instance = new Main();
 
