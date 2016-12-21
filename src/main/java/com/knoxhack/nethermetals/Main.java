391524b6a95583db8fc7f9773a5713c43cbdf17a
Jasmine Iwanek <jasmine@iwanek.co.uk>
2016-12-21 02:58:01 -0800

    tweaking

--- src/main/java/com/knoxhack/nethermetals/Main.java
+++ src/main/java/com/knoxhack/nethermetals/Main.java
@@ -43,7 +43,7 @@ public class Main {
 
 	public static final String MODID = "nethermetals";
 	public static final String MODNAME = "Nether Metals";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.2.0";
 
 	/** All ore-spawn files discovered in the ore-spawn folder */
 	// public static final List<Path> oreSpawnConfigFiles = new LinkedList<>();
