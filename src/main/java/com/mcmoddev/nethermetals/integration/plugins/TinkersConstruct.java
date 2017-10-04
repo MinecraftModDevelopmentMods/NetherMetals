d5a639a4b24657fa03f7d49e8a7e04dfee3777b3
Daniel Hazelton <dshadowwolf@gmail.com>
2017-10-03 22:20:19 -0400

    Was not using the material names system and was not checking to make sure the material was enabled - corrected that, this should fix a major, potential bug

--- src/main/java/com/mcmoddev/nethermetals/integration/plugins/TinkersConstruct.java
+++ src/main/java/com/mcmoddev/nethermetals/integration/plugins/TinkersConstruct.java
@@ -12,6 +12,7 @@ import com.mcmoddev.lib.init.Materials;
 import com.mcmoddev.lib.integration.IIntegration;
 import com.mcmoddev.lib.integration.MMDPlugin;
 import com.mcmoddev.lib.material.MMDMaterial;
+import com.mcmoddev.lib.util.ConfigBase.Options;
 
 /**
  *
@@ -51,48 +52,51 @@ public class TinkersConstruct extends com.mcmoddev.lib.integration.plugins.Tinke
 	}
 
 	private void registerMelting() {
-		registerExtraMeltingWrapper(Materials.getMaterialByName("coal"), 576);
-		registerExtraMeltingWrapper(Materials.getMaterialByName("diamond"), 576);
-		registerExtraMeltingWrapper(Materials.getMaterialByName("emerald"), 576);
-		registerExtraMeltingWrapper(Materials.getMaterialByName("gold"), 576);
-		registerExtraMeltingWrapper(Materials.getMaterialByName("iron"), 576);
+		registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.COAL), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.COAL), 576);
+		registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.DIAMOND), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.DIAMOND), 576);
+		registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.EMERALD), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.EMERALD), 576);
+		registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.GOLD), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.GOLD), 576);
+		registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.IRON), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.IRON), 576);
 		// No such thing as a Lapis fluid in the code
 		//registerExtraMeltingWrapper(Materials.getMaterialByName("lapis"), 576);
-		registerExtraMeltingWrapper(Materials.getMaterialByName("redstone"), 576);
+		registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.REDSTONE), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.REDSTONE), 576);
 		
 		if( Loader.isModLoaded("basemetals") ) {
-			registerExtraMeltingWrapper(Materials.getMaterialByName("antimony"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("bismuth"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("copper"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("lead"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("mercury"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("nickel"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("platinum"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("silver"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("tin"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("zinc"), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.ANTIMONY), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.ANTIMONY), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.BISMUTH), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.BISMUTH), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.COPPER), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.COPPER), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.LEAD), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.LEAD), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.MERCURY), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.MERCURY), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.NICKEL), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.NICKEL), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.PLATINUM), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.PLATINUM), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.SILVER), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.SILVER), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.TIN), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.TIN), 576);
+			registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.basemetals.data.MaterialNames.ZINC), Materials.getMaterialByName(com.mcmoddev.basemetals.data.MaterialNames.ZINC), 576);
 		}
 		
 		if( Loader.isModLoaded("modernmetals") ) {
-			registerExtraMeltingWrapper(Materials.getMaterialByName("aluminum"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("cadmium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("chromium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("iridium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("magnesium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("manganese"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("osmium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("plutonium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("rutile"),  576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("tantalum"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("titanium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("tungsten"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("uranium"), 576);
-			registerExtraMeltingWrapper(Materials.getMaterialByName("zirconium"), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.ALUMINUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.ALUMINUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.CADMIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.CADMIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.CHROMIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.CHROMIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.IRIDIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.IRIDIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.MAGNESIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.MAGNESIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.MANGANESE),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.MANGANESE), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.OSMIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.OSMIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.PLUTONIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.PLUTONIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.RUTILE),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.RUTILE),  576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.TANTALUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.TANTALUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.TITANIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.TITANIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.TUNGSTEN),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.TUNGSTEN), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.URANIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.URANIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.ZIRCONIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.ZIRCONIUM), 576);
+            registerExtraMeltingWrapper(Options.isMaterialEnabled(com.mcmoddev.modernmetals.data.MaterialNames.ZIRCONIUM),Materials.getMaterialByName(com.mcmoddev.modernmetals.data.MaterialNames.ZIRCONIUM), 576);
 		}
 	}
 
-	private static void registerExtraMeltingWrapper(MMDMaterial material, int quantity) {
-		FluidStack result = FluidRegistry.getFluidStack(material.getName(), quantity);
-		registry.registerMelting(Item.getItemFromBlock(material.getBlock(Names.NETHERORE)), result);
-	}	
+	private static void registerExtraMeltingWrapper(boolean enabled, MMDMaterial material, int quantity) {
+		if( enabled ) {
+			FluidStack result = FluidRegistry.getFluidStack(material.getName(), quantity);
+			registry.registerMelting(Item.getItemFromBlock(material.getBlock(Names.NETHERORE)), result);
+		}	
+	}
 }
