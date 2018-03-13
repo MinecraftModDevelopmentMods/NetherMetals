b2312880b5101301bf2755e38d943aacc02afce4
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-03-13 00:06:23 -0700

    fix up orespawn

--- src/main/java/com/mcmoddev/orespawn/NetherMetalsOreSpawn.java
+++ src/main/java/com/mcmoddev/orespawn/NetherMetalsOreSpawn.java
@@ -4,7 +4,7 @@ import com.mcmoddev.orespawn.api.os3.OS3API;
 import com.mcmoddev.orespawn.api.plugin.IOreSpawnPlugin;
 import com.mcmoddev.orespawn.api.plugin.OreSpawnPlugin;
 
-@OreSpawnPlugin( modid="nethermetals", resourcePath="orespawn" )
+@OreSpawnPlugin( modid = "nethermetals", resourcePath = "orespawn" )
 public class NetherMetalsOreSpawn implements IOreSpawnPlugin {
 
 	@Override
